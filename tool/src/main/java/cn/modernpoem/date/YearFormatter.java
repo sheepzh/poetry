6d7ee933d2cf4da79a422fa003a7f175f049cef8
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-03-26 18:57:55 +0800

    New date format

--- tool/src/main/java/cn/modernpoem/date/YearFormatter.java
+++ tool/src/main/java/cn/modernpoem/date/YearFormatter.java
@@ -7,9 +7,9 @@ import java.util.regex.Pattern;
  * @author zhy
  */
 public class YearFormatter implements DateFormatter {
-    private final static Pattern PATTERN0 = Pattern.compile("^（?写?于?(\\d{4})[.．/-年]?([于在]?[^\\d]{0,4})?）?$");
+    private final static Pattern PATTERN0 = Pattern.compile("^（?写?于?(\\d{4})[.．、/-年]?([于在]?[^\\d]{0,4})?）?$");
 
-    private final static Pattern PATTERN1 = Pattern.compile("^（?([一二三四五六七八九〇零O0-9]{4})[.．/\\-年]?([于在]?[^\\d]{0,4})?）?$");
+    private final static Pattern PATTERN1 = Pattern.compile("^（?([一二三四五六七八九〇零O0-9]{4})[.、．/\\-年]?([于在]?[^\\d]{0,4})?）?$");
 
     private String pattern0(String lastLine) {
         Matcher matcher = PATTERN0.matcher(lastLine);
