051918c3ceaace35e234b16923b57e299250d04c
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-04-11 20:42:14 +0800

    Clean

--- script/analyze/poetcloud.py
+++ script/analyze/poetcloud.py
@@ -5,7 +5,14 @@ import os
 
 import random
 import numpy as np
+import math
 from pyecharts.charts import WordCloud
+
+adjust = {
+    '陈宗华': 0.05,
+    '槐蓝言白': 0.1,
+    '龙羽生': 0.8
+}
 
 origin_dir_path = path.join('..', '..', 'data')
 
@@ -33,14 +40,19 @@ for poet_path, dir_list, file_list in os.walk(origin_dir_path):
                         frequency = frequency + len(line)
                 # Score of poem number
                 poem_count = poem_count + 1
-        total_word_num = total_word_num + frequency
-        frequencies.append((poet_name, frequency + poem_count * 30))
+        total_word_num = (total_word_num + frequency)
+        frequency = frequency + poem_count * 30
+        if poet_name in adjust:
+            rate = adjust.get(poet_name)
+            print('Adjusted ' + poet_name + ' with ' + str(rate))
+            frequency = math.floor(frequency*rate)
+        frequencies.append((poet_name, frequency))
 
 frequencies.sort(reverse=True, key=lambda bi: bi[1])
-print('Total word number: ' + str(total_word_num))
 frequencies = frequencies[0:200]
 
 print(frequencies)
+print('Total word number: ' + str(total_word_num))
 
 wc = (
     WordCloud().add(shape='circle', series_name="诗人分布",
