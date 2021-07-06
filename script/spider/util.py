c63c153c94c703faa6e4c105a517b1d8a66d19f7
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-07-06 11:04:07 +0800

    Read more

--- script/spider/util.py
+++ script/spider/util.py
@@ -1,4 +1,3 @@
-
 # pip3 install pypinyin
 from pypinyin import lazy_pinyin
 import os
@@ -12,14 +11,14 @@ class Profile:
         self.title = title
 
     def __str__(self):
-        return self.author + ' ' + self.title + ' ' + self.href
+        return self.author + " " + self.title + " " + self.href
 
     def poet_path(self):
         pinyin = "".join(lazy_pinyin(self.author))
-        return os.path.join('.', 'tmp', self.author + '_' + pinyin)
+        return os.path.join(".", "tmp", self.author + "_" + pinyin)
 
     def file_path(self):
-        return os.path.join(self.poet_path(), self.title + '.pt')
+        return os.path.join(self.poet_path(), self.title + ".pt")
 
 
 def write_poem(p, content):
@@ -28,18 +27,19 @@ def write_poem(p, content):
         os.makedirs(poet_dir_path)
     title = p.title
     poem_path = p.file_path()
-    content = 'title:' + title + '\r\n' + 'date:\r\n\r\n' + content
+    content = "title:" + title + "\n" + "date:\n\n" + content
     try:
-        with open(poem_path, 'w') as file:
+        with open(poem_path, "w", encoding="utf-8") as file:
             file.write(content)
     except OSError:
-        print('author:' + p.author + '\r\n')
-        print('title:' + title + '\r\n')
-        print('-----------------------')
+        print("author:" + p.author + "\r\n")
+        print("title:" + title + "\r\n")
+        print("href:" + p.href + "\r\n")
+        print("-----------------------")
 
 
 def remove_tmp_all():
     try:
-        shutil.rmtree(os.path.join('.', 'tmp'))
+        shutil.rmtree(os.path.join(".", "tmp"))
     except FileNotFoundError:
         pass
