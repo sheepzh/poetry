c77708766d41122f06650f346f2256b5972e1c89
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-07-19 16:11:23 +0800

    Update rate

--- script/analyze/adjust_rate.py
+++ script/analyze/adjust_rate.py
@@ -3,6 +3,7 @@
     通过搜索引擎统计
 """
 import asyncio
+from genericpath import exists
 from pyppeteer import launch
 from pyquery import PyQuery as pq
 import re
@@ -20,6 +21,11 @@ google_reg = re.compile(r'找到约\s?([\d,]*)\s?条结果')
 google_reg2 = re.compile(r'获得\s([\d,]*)\s条结果')
 google_scholar_reg = re.compile(r'找到约\s([\d,]*)\s条结果')
 google_scholar_reg2 = re.compile(r'获得\s([\d,]*)\s条结果')
+
+temp_file_path = 'search_count.tmp'
+
+if os.path.exists(temp_file_path):
+    os.remove(temp_file_path)
 
 
 async def search(browser, name, url, id, reg, reg2=None):
@@ -44,30 +50,31 @@ async def search(browser, name, url, id, reg, reg2=None):
 
 async def main():
 
-    exist = {}
-    if not os.path.exists('search_count.csv'):
-        file = open('search_count.csv', 'a', encoding='utf-8')
-    else:
+    exists = {}
+    to_write = []
+    if os.path.exists('search_count.csv'):
         file = open('search_count.csv', 'r', encoding='utf-8')
         lines = file.readlines()
         if len(lines):
             for line in lines[1:]:
                 segs = line.split(',')
-                exist[segs[0].strip()] = segs[1].strip()
+                name = segs[0].strip()
+                score = segs[1].strip()
+                exists[name] = score
             file.close()
-        else:
-            file.close()
-            file = open('search_count.csv', 'a', encoding='utf-8')
-            file.write('name, google_scholar', encoding='utf-8')
-            file.close()
+
     data_path = os.path.join('..', '..', 'data')
+    searched = 0
     for dir_ in os.listdir(data_path):
         if '_' not in dir_:
             continue
+        searched += 1
+        print('start to search {}th, {}'.format(searched, dir_))
 
         index = dir_.rindex('_')
         name_query = dir_[:index]
-        if name_query in exist:
+        if name_query in exists:
+            to_write.append((name_query, exists[name_query]))
             continue
 
         browser = await launch(headless=True, ignoreDefaultArgs=['--enable-automation'], args=["--proxy=127.0.0.1:8001"])
@@ -78,11 +85,20 @@ async def main():
             quit()
         line = '{},{}'.format(name_query, google_scholar_count)
         print(line)
+        temp_file = open(temp_file_path, 'a', encoding='utf-8')
+        temp_file.write(line + '\n')
+        temp_file.close()
 
-        file = open('search_count.csv', 'a', encoding='utf-8')
-        file.write('\n{}'.format(line))
-        file.close()
-        # time.sleep(10)
+        to_write.append((name_query, int(google_scholar_count)))
         browser.close()
 
+    to_write = sorted(to_write, key=lambda e: int(e[1]), reverse=True)
+
+    file = open('search_count.csv', 'w', encoding='utf-8')
+    file.write('name, google_scholar')
+    for line in to_write:
+        file.write('\n{},{}'.format(line[0], line[1]))
+    file.close()
+    if os.path.exists(temp_file_path):
+        os.remove(temp_file_path)
 asyncio.get_event_loop().run_until_complete(main())
