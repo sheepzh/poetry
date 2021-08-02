f3c4c2076be7a403c84cafb06d6b668232d114db
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-08-02 11:45:06 +0800

    Update description by bot

--- update_count.py
+++ update_count.py
@@ -37,13 +37,45 @@ readme_filename = 'README.md'
 readme = open(readme_filename, 'r', encoding='utf-8').read()
 
 # Update the count
+poet_count = format_digit(poet)
 poet_re = re.compile(r'badge/poets-(.*)-')
-readme = poet_re.sub('badge/poets-{}-'.format(format_digit(poet)), readme)
+readme = poet_re.sub('badge/poets-{}-'.format(poet_count), readme)
 
+poem_count = format_digit(poem, 'K')
 poem_re = re.compile(r'badge/poems-(.*)-')
-readme = poem_re.sub('badge/poems-{}-'.format(format_digit(poem, 'K')), readme)
+readme = poem_re.sub('badge/poems-{}-'.format(poem_count), readme)
 
+word_count = format_digit(word, 'M')
 word_re = re.compile(r'badge/words-(.*)-')
-readme = word_re.sub('badge/words-{}-'.format(format_digit(word, 'M')), readme)
+readme = word_re.sub('badge/words-{}-'.format(word_count), readme)
 
 open(readme_filename, 'w', encoding='utf-8').write(readme)
+
+# Update this description
+# @see
+# https://docs.github.com/en/rest/reference/repos?query=description#update-a-repository
+auth = os.getenv('GITHUB_AUTH')
+if not auth:
+    print("No github auth set")
+    quit()
+else:
+    print("GITHUB_AUTH={}".format(auth))
+
+desc = "最全的汉语现代诗歌语料库整理，{}诗人，{}诗歌，{}字，包括五四至今的所有流派。持续扩充...".format(poet_count, poem_count, word_count)
+import requests
+import json
+data = {
+    "description": desc
+}
+
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Content-Type": "application/json;charset:utf-8;",
+    "Authorization": "token {}".format(auth)
+}
+
+response = requests.patch('https://api.github.com/repos/sheepzh/poetry', data=json.dumps(data), headers=headers)
+if(response.status_code != 200):
+    print("Failed to update description: {}".format(response.text))
+else:
+    print("Updated description")
