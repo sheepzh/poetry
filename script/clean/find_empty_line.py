ea019499867553430f630f55b23a0fef765c17fd
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-08-21 01:15:21 +0800

    Find empty lines

---
+++ script/clean/find_empty_line.py
@@ -0,0 +1,37 @@
+"""
+    To find the empty_line
+"""
+
+from util import Poem, iterate_poems, write_poem
+import os
+import sys
+
+
+argv = sys.argv
+if len(argv) > 1:
+    path = argv[1]
+
+if not os.path.exists(path):
+    print('no directory')
+    quit()
+
+
+def parse_poem(poem: Poem, file_path, __file_name__):
+    content = poem.content
+    if len(content) <= 2:
+        return
+    prev_empty = True
+    need_remove_empty = True
+    for line in content:
+        if line and not prev_empty:
+            need_remove_empty = False
+            break
+        prev_empty = not bool(line)
+    if need_remove_empty:
+        print(file_path)
+        content = list(map(lambda line: line.replace('\n', '').replace('\r', ''), filter(lambda line: line.strip(), content)))
+        poem.content = content
+    write_poem(poem, file_path)
+
+
+iterate_poems(path, parse_poem)
