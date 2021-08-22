d3d3e305fd46e75c01221eb55934ac28f72603c5
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-08-22 13:14:45 +0800

    New date format

--- tool/src/main/java/cn/modernpoem/date/YmdFormatter.java
+++ tool/src/main/java/cn/modernpoem/date/YmdFormatter.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 public class YmdFormatter implements DateFormatter {
     private final static Pattern PATTERN = Pattern.compile("^[(（]?\\s*(\\d{4})[.,、′，．/╱\\-年]?(\\d{1,2})[.,、′，．/╱\\-月](\\d{1,2})[.．日]?[，于在]?.{0,4}\\s*[)）]?$");
     private final static Pattern PATTERN_1 = Pattern.compile("([零一二三四五六七八九十〇]{4})年([一二三四五六七八九十〇]{1,2})月([一二三四五六七八九十〇廿卅]{1,3})日");
+    private final static Pattern PATTERN_2 = Pattern.compile("^((19|20)\\d{2}[0-1]\\d[0-3]\\d)$");
 
     @Override
     public String format(String lastLine) {
@@ -42,6 +43,10 @@ public class YmdFormatter implements DateFormatter {
                     .chars().mapToObj(DateFormatter::toNumber)
                     .collect(Collectors.joining());
             return year + DateFormatter.addZeroOf(month) + DateFormatter.addZeroOf(date);
+        }
+        matcher = PATTERN_2.matcher(lastLine);
+        if (matcher.matches()) {
+            return matcher.group();
         }
         return null;
     }
